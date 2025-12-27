1db0f5c396ebf8241196a04e4f9be9c67dc1fe47
Pushpendra Singh <pushpendrasingh0549@gmail.com>
2025-12-27 11:33:08 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,3 +1,7 @@
-def call(){
-  sh "docker run -d --name ps-app -p 8000:8000 notes-app:latest --address=0.0.0.0
+def call() {
+    sh '''
+    docker rm -f ps-app || true
+    docker run -d --name ps-app -p 8000:8000 notes-app:latest
+    '''
 }
+
