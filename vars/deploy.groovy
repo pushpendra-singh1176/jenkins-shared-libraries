aa828b96b94e8f2c2f24d8b6c1943cdf4f75a0d8
Pushpendra Singh <pushpendrasingh0549@gmail.com>
2025-12-27 17:11:33 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,7 +1,7 @@
 def call() {
     sh '''
     docker rm -f ps-app || true
-    docker run -d -p 8000:8000 xerox2/notes-app:latest
+    docker run -d --name ps-app -p 8000:8000 xerox2/notes-app:latest
     
     '''
 }
