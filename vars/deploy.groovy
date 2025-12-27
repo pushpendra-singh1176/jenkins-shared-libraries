3739318008faaef38389847d5b7aa8693c5519eb
Pushpendra Singh <pushpendrasingh0549@gmail.com>
2025-12-27 17:25:40 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,8 +1,15 @@
 def call() {
     sh '''
     docker rm -f ps-app || true
-    docker run -d --name ps-app -p 8000:8000 xerox2/notes-app:latest
-    
-    '''
+    docker run -d \
+  --name ps-app \
+  -p 8000:8000 \
+  --link mysql-db \
+  -e DB_NAME=notesdb \
+  -e DB_USER=notesuser \
+  -e DB_PASSWORD=notespwd \
+  -e DB_HOST=mysql-db \
+  xerox2/notes-app:latest
+ '''
 }
 
